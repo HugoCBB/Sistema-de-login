2ef8eb8539a428834406347c33cd38dc4d0caed0
Hugo B <157296697+HugoBrandaoo@users.noreply.github.com>
2024-02-25 18:53:57 -0300

    Update Sistema-de-login
    Para esse update, aprendi como adicionar inputs e botões dentro de um container. Foram adicionado os inputs e o botão de login, nesse update + a cor do frame, largura, altura e a borda.

--- login.py
+++ login.py
@@ -1,9 +1,4 @@
 import flet as ft
-cont = ft.Container(
-        width= 470,
-        height= 240,
-        bgcolor= '#FFFFFF',
-   )
 def main(page: ft.Page):
     page.title = 'Login'
     page.bgcolor = '#2F6DE3'
@@ -13,24 +8,34 @@ def main(page: ft.Page):
     page.window_resizable = False
     page.vertical_alignment = 'center'
     page.horizontal_alignment = 'center'
+    
     page.add(
-        cont,
-        ft.Row(
-            controls=[
-                ft.TextField(label='Digite seu email')
-            ]
-        ),
-    )
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        ft.Container(
+            width= 470,
+            height= 240,
+            bgcolor= 'WHITE',
+            border_radius= 5,
+            content= ft.Column(
+                controls=[
+                ft.TextField(
+                    label= 'Digite seu email',
+                    width= 300,
+                    height= 50,
+                    bgcolor= None,
+                    text_align= 'Left',
+                    color= 'Black',
+                    ),
+                ft.TextField(
+                    label= 'Digite sua senha',
+                    width= 300,
+                    height= 50,
+                    bgcolor= None,
+                    text_align= 'Left',
+                    color= 'Black'
+                    ),
+                ft.ElevatedButton(
+                    text= 'Enter',
+                    )
+                    ])))
     page.update()
-
-
 ft.app(target=main)
