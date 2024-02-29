78982fe8781acc85fb5ffb2f421b15e2d3d3a9f2
Hugo B <157296697+HugoBrandaoo@users.noreply.github.com>
2024-02-28 22:42:31 -0300

    Update sistema-de-login
    Nesse update, finalizei a parte do front, ficou um pouco estranho, mas ficou do jeito que queria. Agora, irei aprender mais sobre a biblioteca request, para poder ser usado nessa aplicação ultilizanodo Flet + Python

--- login.py
+++ login.py
@@ -1,14 +1,22 @@
 import flet as ft
+
 def main(page: ft.Page):
     page.title = 'Login'
     page.bgcolor = '#2F6DE3'
     page.window_width = 500
     page.window_height = 300
-    page.window_maximizable = False
-    page.window_resizable = False
+    page.window_maximizable = True
+    page.window_resizable = True
     page.vertical_alignment = 'center'
     page.horizontal_alignment = 'center'
-    
+    dlg = ft.AlertDialog(
+        title=ft.Text("Login realizado com sucesso!")
+    )
+    def login(e):
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
+        
     page.add(
         ft.Container(
             width= 470,
@@ -16,25 +24,31 @@ def main(page: ft.Page):
             bgcolor= 'WHITE',
             border_radius= 5,
             content= ft.Column(
-                controls=[
-                ft.TextField(
-                    label= 'Digite seu email',
-                    width= 300,
-                    height= 50,
-                    bgcolor= None,
-                    text_align= 'Left',
-                    color= 'Black',
-                    ),
-                ft.TextField(
-                    label= 'Digite sua senha',
-                    width= 300,
-                    height= 50,
-                    bgcolor= None,
-                    text_align= 'Left',
-                    color= 'Black'
-                    ),
-                ft.ElevatedButton(
-                    text= 'Enter',
+                horizontal_alignment= 'center',
+                alignment= 'center',
+                controls= [
+                    ft.TextField(
+                        label= 'Digite seu email',
+                        width= 300,
+                        height= 50,
+                        text_align= 'Left',
+                        color= 'Black',
+                        border_radius= 15
+                        ),
+                        
+                    ft.TextField(
+                        label= 'Digite sua senha',
+                        width= 300,
+                        height= 50,
+                        text_align= 'Left',
+                        color= 'Black',
+                        border_radius= 15,
+                        password= True,
+                        can_reveal_password= True
+                        ),
+                    ft.ElevatedButton(
+                        text= 'Login',
+                        on_click= login
                     )
                     ])))
     page.update()
